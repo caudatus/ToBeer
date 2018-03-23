80123093e8028ddec3665a7c709d6d7b6b4acc84
caudatus <jaehyeongee@gmail.com>
2018-03-23 23:15:57 +0900

    add Project

---
+++ ToBeer/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToBeer
+//
+//  Created by Seo JaeHyeong on 23/03/2018.
+//  Copyright © 2018 Seo Jaehyeong. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
---
+++ ToBeer/Model/BeerModel.swift
@@ -0,0 +1,9 @@
+//
+//  BeerModel.swift
+//  ToBeer
+//
+//  Created by Seo JaeHyeong on 23/03/2018.
+//  Copyright © 2018 Seo Jaehyeong. All rights reserved.
+//
+
+import Foundation
